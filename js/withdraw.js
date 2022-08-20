8dd8e13163b9250508876c6d0a74415fd12d8334
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-20 14:01:37 +0600

    first commit

---
+++ js/withdraw.js
@@ -0,0 +1,45 @@
+// step 1: add event handler with the withdraw button
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+   
+    // step 2: get the withdraw amount from the input withdraw field and also convert String to int by using parseFloat.
+
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step 7: clear the withdraw field
+    withdrawField.value = ' ';
+    
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number!');
+        return;
+    }
+
+    // step 3: get previous withdraw total
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+
+    // step 5: set total withdraw amount
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap er bank e eto taka nai');
+        return;
+    }
+
+    // step 4: calculate total withdraw amount
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+ 
+    // step 6: calculate new balance total and set the new balance total
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+
+})
